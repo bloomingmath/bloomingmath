3edadb2195922f02b7db1052ed8006161077a7af
Matteo Bortolotto <yamatteo@gmail.com>
2019-08-14 13:55:00 -0400

    Automatic commit

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m "Automatic commit"
 git push
 
-ssh bloomingmath@ssh.pythonanywhere.com "cd mysite | git pull"
+ssh bloomingmath@ssh.pythonanywhere.com "cd mysite && git pull"
 
 source venv/bin/activate
 python reload.py
